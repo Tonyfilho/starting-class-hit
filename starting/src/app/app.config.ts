d1e146c9d799284ebcb65c702d6af5cb2136c130
tonfi filho <oficinadosdrones@gmail.com>
2025-05-30 12:18:59 +0100

    fixing aula04

--- starting/src/app/app.config.ts
+++ starting/src/app/app.config.ts
@@ -3,23 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCVgB-D0ltk6-XqhA84MMd-vPSUQbTiHkI",
-  authDomain: "starting-project-hit-tony.firebaseapp.com",
-  projectId: "starting-project-hit-tony",
-  storageBucket: "starting-project-hit-tony.firebasestorage.app",
-  messagingSenderId: "683438427469",
-  appId: "1:683438427469:web:6f392e12e0f65368103b04"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
