20803eaabc1433de792555073175a08932e7137c
tonfi filho <oficinadosdrones@gmail.com>
2025-06-03 09:33:49 +0100

    inicio da aula06 crianção dos components

---
+++ starting/src/app/pages/books/level/level01/control-structures/control-structures.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-structures',
+  imports: [],
+  templateUrl: './control-structures.component.html',
+  styleUrl: './control-structures.component.css'
+})
+export class ControlStructuresComponent {
+
+}
--- starting/src/app/pages/books/level/level01/level01.component.ts
+++ starting/src/app/pages/books/level/level01/level01.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { ControlStructuresComponent } from "./control-structures/control-structures.component";
+import { OperatorsComponent } from "./operators/operators.component";
+import { DependencyInjectionComponent } from "./dependency-injection/dependency-injection.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-level01',
-  imports: [],
+  imports: [ControlStructuresComponent, OperatorsComponent, DependencyInjectionComponent, CommonModule],
   templateUrl: './level01.component.html',
   styleUrl: './level01.component.css'
 })
