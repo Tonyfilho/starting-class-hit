ac541035c853ad61b3ac9481b6ef4eade20e8512
tonfi filho <oficinadosdrones@gmail.com>
2025-06-06 11:34:36 +0100

    criação do html e css git.component.css git.component.html

--- starting/src/app/app.routes.ts
+++ starting/src/app/app.routes.ts
@@ -8,19 +8,12 @@ export const routes: Routes = [
   { path: 'angular/git', loadComponent: () => import('./pages/angular/git/git.component').then(c => c.GitComponent) },
   { path: 'angular/hit', loadChildren: () => import('./pages/hit/hit.routes').then(c => c.hitRoutes) },
   { path: 'angular/levels', loadChildren: () => import('./pages/books/level/level.router').then(c => c.levelRoutes) },
-
-
-
-
-
-
   { path: '**', loadComponent: () => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent) }
   // {path: '**', redirectTo: () => {return 'not-found'}},
 
 ];
 export const routesExemplo: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-
   { path: '**', component: NotFoundComponent }
 
 
