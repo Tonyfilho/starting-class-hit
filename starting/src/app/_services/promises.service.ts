0d3bdae51222e8adf0374484b3a067d42a04f8e0
tonfi filho <oficinadosdrones@gmail.com>
2025-06-09 14:59:03 +0100

    promises.component done

--- starting/src/app/_services/promises.service.ts
+++ starting/src/app/_services/promises.service.ts
@@ -4,17 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PromisesService {
-protected promise  = new Promise(resolve => resolve('Nossa Promise !'));
-promiseValue: any = null;
+  protected promise = new Promise(resolve => resolve('Nossa Promise !'));
+  promiseValue: any = null;
   constructor() {
     this.promise.then(valeu => console.log("Promise in Service: ", valeu));
-   }
+  }
 
 
-getPromise() {
-  this.promise.then(res => this.promiseValue = res + ' Agora foi modificada');
-}
+  getPromise() {
+    this.promise.then(res => this.promiseValue = res + ' Agora foi modificada');
+  }
 
+
+  seekRandomicData = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      // Simulando uma operação assíncrona (como uma chamada HTTP)
+      setTimeout(() => {
+        const sucess = Math.random() > 0.3; //70% de chance de sucesso
+        if (!sucess) {
+          reject('Erro: Falha ao carregar dados!');
+        }
+          resolve('Dados carregados com sucesso!')
+
+      },1500); // Atraso de 1.5 segundos para simular operação assíncrona
+
+    });
+  }
 
 
 }
--- starting/src/app/pages/books/level/level02/promises/promises.component.ts
+++ starting/src/app/pages/books/level/level02/promises/promises.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PromisesService } from '../../../../../_services/promises.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-promises',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './promises.component.html',
   styleUrl: './promises.component.css'
 })
@@ -12,16 +13,77 @@ export class PromisesComponent implements OnInit {
 
   protected promisesService = inject(PromisesService);
   protected localPromise: any;
+  protected localLoad = false;
+  protected mensagem: string = '';
+  protected erro: string = '';
 
 
 
   ngOnInit(): void {
     console.log("Promise in Component: ");
     console.log("Promise in Component: ", this.promisesService.getPromise());
-
     console.log("promiseValue: ", this.promisesService.promiseValue);
 
   }
+  /*********************2º Exemplo */
+  loadData() {
+    this.localLoad = true;
+    this.promisesService.seekRandomicData().then((res) => {
+      this.mensagem = res;
+      this.localLoad = false;
+    }).catch((err) => {
+      this.erro = err;
+      this.localLoad = false;
+    });
 
+
+  }
+
+
+  /**
+   * Método async:
+
+ O método carregarDados() é marcado com async, o que permite usar await dentro dele
+
+ Isso faz com que o método retorne implicitamente uma Promise
+
+ Sintaxe await:
+
+ Substitui o .then(), fazendo o código esperar até a Promise ser resolvida
+
+ O código parece síncrono, mas mantém o comportamento assíncrono
+
+ Tratamento de erros:
+
+ Usamos try/catch em vez de .catch()
+
+ Mais natural e similar ao tratamento de erros síncrono
+
+ Adicionei um bloco finally que sempre executa, independente de sucesso ou erro
+
+ Vantagens:
+
+ Código mais limpo e linear
+
+ Mais fácil de ler e entender
+
+ Melhor encadeamento de operações assíncronas
+
+ Tratamento de erros mais intuitivo
+   */
+
+  async loadData2() {
+
+    this.localLoad = true;
+    try {
+      this.mensagem = await this.promisesService.seekRandomicData();
+
+    } catch (error) {
+      this.erro = error as string;
+    } finally {
+      this.localLoad = false;
+
+    }
+  }
 
 }
