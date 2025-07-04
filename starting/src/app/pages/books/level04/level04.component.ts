536a1eff8818434209e5d7e972cf72cd87309b73
tonfi filho <oficinadosdrones@gmail.com>
2025-07-04 10:24:36 +0100

    level04.component done

--- starting/src/app/pages/books/level04/level04.component.ts
+++ starting/src/app/pages/books/level04/level04.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
-import { PromisesComponent } from "./promises/promises.component";
-import { ObservablesComponent } from "./observable01/observables.component";
-import { SignalComponent } from "./signal/signal.component";
-import { Observable02Component } from "./observable02/observable02.component";
-import { RouterLink, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { UpcaseFirstWordPipe } from '../../../_shared/pipes/upcase-first-word.pipe';
+import { ObservablesComponent } from "./observable01/observables.component";
+import { Observable02Component } from "./observable02/observable02.component";
+import { PromisesComponent } from "./promises/promises.component";
+import { SignalComponent } from "./signal/signal.component";
 
 @Component({
   selector: 'app-level04',
   imports: [PromisesComponent, ObservablesComponent, Observable02Component, SignalComponent, RouterModule, CommonModule, UpcaseFirstWordPipe],
   templateUrl: './level04.component.html',
-  styleUrl: './level04.component.css'
+  styleUrl: './../../angular/angular.component.css'
 })
 export class Level04Component {
 
