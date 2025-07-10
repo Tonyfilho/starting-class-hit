dd692c76e1fde943fd6edd45e1bcdadb48d36d16
tonfi filho <oficinadosdrones@gmail.com>
2025-07-10 20:05:16 +0100

    functions-and-scope.functions-and-scope.component.html

---
+++ starting/src/app/pages/js-fundaments/02-functions-and-scope/functions-and-scope.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-functions-and-scope',
+  imports: [],
+  templateUrl: './functions-and-scope.component.html',
+  styleUrl: './../../angular/angular.component.css'
+})
+export class FunctionsAndScopeComponent {
+
+}
--- starting/src/app/pages/js-fundaments/js-fundaments.router.ts
+++ starting/src/app/pages/js-fundaments/js-fundaments.router.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const jsFundamentsRoutes: Routes = [
     {path: '', redirectTo: 'js-fundaments', pathMatch: 'full'},
     {path: '', loadComponent:() => import('./js-fundaments.component').then(c => c.JsFundamentsComponent)},
-    {path: 'js-fundamentals', loadComponent:() => import('./js-fundamentals/js-fundamentals.component').then(c => c.JsFundamentalsComponent)},
+    {path: 'js-fundamentals', loadComponent:() => import('./01-js-fundamentals/js-fundamentals.component').then(c => c.JsFundamentalsComponent)},
+    {path: 'functions-and-scope', loadComponent:() => import('./02-functions-and-scope/functions-and-scope.component').then(c => c.FunctionsAndScopeComponent)},
 
 ]
