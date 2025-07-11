3a80ede9693408f88cbabb2a8e6cb7ec26deb49e
tonfi filho <oficinadosdrones@gmail.com>
2025-07-11 10:06:55 +0100

    object-and-arrays.component.ts object-and-arrays.component.html object-and-arrays.component.html

---
+++ starting/src/app/pages/js-fundaments/03-object-and-arrays/object-and-arrays.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-object-and-arrays',
+  imports: [],
+  templateUrl: './object-and-arrays.component.html',
+  styleUrl: './../../angular/angular.component.css'
+})
+export class ObjectAndArraysComponent {
+
+}
--- starting/src/app/pages/js-fundaments/js-fundaments.router.ts
+++ starting/src/app/pages/js-fundaments/js-fundaments.router.ts
@@ -5,5 +5,6 @@ export const jsFundamentsRoutes: Routes = [
     {path: '', loadComponent:() => import('./js-fundaments.component').then(c => c.JsFundamentsComponent)},
     {path: 'js-fundamentals', loadComponent:() => import('./01-js-fundamentals/js-fundamentals.component').then(c => c.JsFundamentalsComponent)},
     {path: 'functions-and-scope', loadComponent:() => import('./02-functions-and-scope/functions-and-scope.component').then(c => c.FunctionsAndScopeComponent)},
+    {path: 'object-and-arrays', loadComponent:() => import('./03-object-and-arrays/object-and-arrays.component').then(c => c.ObjectAndArraysComponent)},
 
 ]
