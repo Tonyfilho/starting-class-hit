ef1dfb901c86b145318de4bd31febaa409ccadc4
tonfi filho <oficinadosdrones@gmail.com>
2025-07-12 11:48:19 +0100

    operator-filter.component

---
+++ starting/src/app/pages/rxjs/05-operator-filter/operator-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-operator-filter',
+  imports: [RouterLink],
+  templateUrl: './operator-filter.component.html',
+  styleUrl: './../../angular/angular.component.css'
+})
+export class OperatorFilterComponent {
+
+}
--- starting/src/app/pages/rxjs/rxjs-fundaments.router.ts
+++ starting/src/app/pages/rxjs/rxjs-fundaments.router.ts
@@ -3,10 +3,11 @@ import { Routes } from '@angular/router';
 export const rxJsFundamentsRoutes: Routes = [
     {path: '', redirectTo: 'rxjs-fundaments', pathMatch: 'full'},
     {path: '', loadComponent:() => import('./rxjs.component').then(c => c.RxjsComponent)},
-    {path: 'pipe', loadComponent:() => import('./01-operador-pipe/operator-pipe.component').then(c => c.OperatorPipeComponent)},
-    {path: 'from', loadComponent:() => import('./02-operador-from/operator-from.component').then(c => c.OperatorFromComponent)},
-    {path: 'of', loadComponent:() => import('./03-operador-of/operator-of.component').then(c => c.OperatorOfComponent)},
-    {path: 'map', loadComponent:() => import('./04-operador-map/operator-map.component').then(c => c.OperatorMapComponent)},
+    {path: 'pipe', loadComponent:() => import('./01-operator-pipe/operator-pipe.component').then(c => c.OperatorPipeComponent)},
+    {path: 'from', loadComponent:() => import('./02-operator-from/operator-from.component').then(c => c.OperatorFromComponent)},
+    {path: 'of', loadComponent:() => import('./03-operator-of/operator-of.component').then(c => c.OperatorOfComponent)},
+    {path: 'map', loadComponent:() => import('./04-operator-map/operator-map.component').then(c => c.OperatorMapComponent)},
+    {path: 'filter', loadComponent:() => import('./05-operator-filter/operator-filter.component').then(c => c.OperatorFilterComponent)},
 
 
 ]
