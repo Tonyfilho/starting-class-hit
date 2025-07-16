44292fd4f7d528041a7ad20e33a005b0c9851ded
tonfi filho <oficinadosdrones@gmail.com>
2025-07-16 18:36:13 +0100

    routing.component

--- starting/src/app/pages/books/level01/level01.router.ts
+++ starting/src/app/pages/books/level01/level01.router.ts
@@ -8,7 +8,6 @@ export const level01Routes: Routes = [
   { path: 'operators', loadComponent: () => import('./03-operators/operators.component').then(cChild => cChild.OperatorsComponent) },
   { path: 'pipes', loadComponent: () => import('./04-pipes/pipes.component').then(cChild => cChild.PipesComponent) },
   { path: 'dependency-injection', loadComponent: () => import('./05-dependency-injection/dependency-injection.component').then(cChild => cChild.DependencyInjectionComponent) },
-  // { path: 'leveltwo', loadComponent: () => import('../level02/level02.component').then(cChild => cChild.Level02Component) },
-  // { path: 'levelthree', loadComponent: () => import('../level03/level03.component').then(cChild => cChild.Level03Component) },
-  // { path: 'levelfour', loadComponent: () => import('../level04/level04.component').then(cChild => cChild.Level04Component) },
+  { path: 'routing', loadComponent: () => import('./06-routing/routing.component').then(cChild => cChild.RoutingComponent) },
+
 ];
