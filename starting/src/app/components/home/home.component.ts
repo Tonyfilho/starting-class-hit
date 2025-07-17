331cbd3f3145de1e554b52e93073d83b1fe247f1
tonfi filho <oficinadosdrones@gmail.com>
2025-07-17 16:48:03 +0100

    update home.component.css

--- starting/src/app/components/home/home.component.ts
+++ starting/src/app/components/home/home.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './../../pages/angular/angular.component.css'
 })
 export class HomeComponent {
+ @ViewChild('pdfContent') content!: ElementRef;
 
+  downloadPDF() {
+    const options = {
+      filename: 'angular-guia.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+
+    html2pdf().set(options).from(this.content.nativeElement).save();
+  }
 }
