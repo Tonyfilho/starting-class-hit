85873ba0d3081681c685733fbc5e03c9231d740a
tonfi filho <oficinadosdrones@gmail.com>
2025-07-19 01:19:52 +0100

    tela wellcome.component.css tem erro no responsivo

--- starting/src/app/components/wellcome/wellcome.component.ts
+++ starting/src/app/components/wellcome/wellcome.component.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthGmailService } from '../../_services/auth-gmail.service';
 import { Observable } from 'rxjs';
 import { User } from 'firebase/auth';
+import { UpcaseFirstWordPipe } from "../../_shared/pipes/upcase-first-word.pipe";
 
 @Component({
   selector: 'app-wellcome',
-  imports: [CommonModule],
+  imports: [CommonModule, UpcaseFirstWordPipe],
   templateUrl: './wellcome.component.html',
   styleUrl: './wellcome.component.css'
 })
 export class WellcomeComponent {
- private authGmailService = inject(AuthGmailService);
+  private authGmailService = inject(AuthGmailService);
+  protected router = inject(Router);
   protected localuser: Observable<User | null> = this.authGmailService.getUser();
+
+
 }
