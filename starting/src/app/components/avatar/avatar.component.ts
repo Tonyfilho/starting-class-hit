2182206d8844d3f7015d82200c8e68852949b9d0
tonfi filho <oficinadosdrones@gmail.com>
2025-07-20 11:53:44 +0100

    update avatar.component.html

--- starting/src/app/components/avatar/avatar.component.ts
+++ starting/src/app/components/avatar/avatar.component.ts
@@ -3,10 +3,11 @@ import { Component, inject } from '@angular/core';
 import { AuthGmailService } from '../../_services/auth-gmail.service';
 import { Observable } from 'rxjs';
 import { User } from 'firebase/auth';
+import { UpcaseFirstWordPipe } from "../../_shared/pipes/upcase-first-word.pipe";
 
 @Component({
   selector: 'app-avatar',
-  imports: [CommonModule],
+  imports: [CommonModule, UpcaseFirstWordPipe],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.css'
 })
