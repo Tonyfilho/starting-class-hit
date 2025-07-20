cabe481ea5eff4616d15da0154afb527d9ea1e8e
tonfi filho <oficinadosdrones@gmail.com>
2025-07-20 19:05:09 +0100

    continuação do level 02

--- starting/src/app/pages/books/level02/02-destructuring/destructuring.component.ts
+++ starting/src/app/pages/books/level02/02-destructuring/destructuring.component.ts
@@ -1,48 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-destructuring',
   imports: [CommonModule],
   templateUrl: './destructuring.component.html',
-  styleUrl: './../../level04/promises/promises.component.css'
+  styleUrl: './../../../angular/angular.component.css'
 })
 export class DestructuringComponent {
   /**A desestruturação (destructuring) é uma sintaxe que permite extrair valores de objetos
    * ou arrays de forma mais concisa. Vou explicar cada parte do seu código: */
-  protected nome!: string;
-  protected idade!: string | number;
-  protected apelido!: string;
-  protected email!: string;
-  protected email2!: string;
-  protected primeiro!: number
-  protected segundo!: number;
+ @ViewChild('pdfContent') content!: ElementRef;
 
-
-  constructor() {
-    const pessoa = {
-      nome: "tony",
-      idade: 40,
-      cidade: 'Santana',
-      apelido: "mutiz",
-      email: ""
+   downloadPDF() {
+    const options = {
+      filename: 'binding-types-guia.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
     };
-    /**Desestruturação com nome iquais */
-    ({ nome: this.nome, idade: this.idade } = pessoa);
-    console.log("Desestruturação com nome iguais:  ", this.nome, this.idade);
 
-    /**Desestruturação com valor padrão */
-    ({apelido: this.apelido, email: this.email = "sem@email.com"} = pessoa);
-    console.log("Desestruturação com valor Padrão:  ", this.apelido, this.email);
-
-    /**Array */
-    const numeros = [10,20,30];
-
-    [this.primeiro, this.segundo] = numeros;
-
-    console.log("Array: 1º: ", this.primeiro, '2º: ', this.segundo);
-
-
+    html2pdf().set(options).from(this.content.nativeElement).save();
   }
 
 
--- starting/src/app/pages/books/level02/level02.router.ts
+++ starting/src/app/pages/books/level02/level02.router.ts
@@ -4,6 +4,7 @@ export const level02Routes: Routes = [
 
   { path: '', redirectTo: 'binding-types', pathMatch: 'full' },
   { path: 'binding-types', loadComponent: () => import('./01-binding-types/binding-types.component').then(cChild => cChild.BindingTypesComponent) },
+  { path: 'destructuring', loadComponent: () => import('./02-destructuring/destructuring.component').then(cChild => cChild.DestructuringComponent) },
 
 
 ];
